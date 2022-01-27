ebded5fb4ad1dce2be52659a45510ce891d5c74d
Aselemi Divine <aselemidivine@gmail.com>
2022-01-27 23:40:44 +0100

    working slider

---
+++ jss/home.js
@@ -0,0 +1,41 @@
+// product slider
+
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimentions = item.getBoundingClientRect();
+    let containerWidth = containerDimentions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+    
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+
+// hamburger menu
+
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('nav-list');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+   })
+}
+
+// slick slider 
+
+
+
