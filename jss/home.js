1e7c99d7a23b86fc1df063a6560d819771979db8
Aselemi Divine <aselemidivine@gmail.com>
2022-01-31 16:09:44 +0100

    added to-top icon

--- jss/home.js
+++ jss/home.js
@@ -32,10 +32,21 @@ if (bar) {
 if (close) {
     close.addEventListener('click', () => {
         nav.classList.remove('active');
-   })
+ 
+    })
 }
 
-// slick slider 
+// scroll to top 
+
+const toTop = document.querySelector (".to-top");
+window.addEventListener("scroll", () => {
+    if  (window.pageYOffset > 100)  {
+        toTop.classList.add("active");
+    }
+    else {
+        toTop.classList.remove ("active");
+    }
+})
 
 
 
